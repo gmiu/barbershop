c4fde0ff81d8e05ad3c777ef6e7c23c1c01be176
miu <miu@adobe.com>
2019-05-02 01:53:52 +0300

    Add hello world app
    * Initial skeleton
    * Don't ignore .flaskenv
    * Main blueprint

---
+++ .flaskenv
@@ -0,0 +1,1 @@
+FLASK_APP=barbershop.py
---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from config import Config
+from logging.handlers import RotatingFileHandler
+import logging
+import os
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    if not app.debug and not app.testing:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/barbershop.log',
+                                           maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s '
+            '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Barbershop startup')
+
+    return app
