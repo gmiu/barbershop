d30ceba094de36407c80d70571bbe7214fb58c8a
avb76 <gabriel.miu.93@gmail.com>
2019-05-02 15:19:21 +0300

    Add HTML base template
    * Bootstrap integration

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,8 @@
 from app.main import bp
+from flask import render_template
 
 
 @bp.route('/')
 @bp.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html', title='Home')
